6c9d764db02395ae77614cf0e9fb754bde67174c
txominpelu <imediavilla@viadeoteam.com>
2014-08-11 18:52:53 +0200

    Added initial test fixture (WIP)

--- test/scheduler/core_test.clj
+++ test/scheduler/core_test.clj
@@ -78,3 +78,23 @@
           framework (framework/createFramework "fr1" [task])]
       (cluster/registerFramework cluster framework)
       (is (= [] (flatten (framework/getClusterTasks (runClusterTillNoTask cluster))))))))
+
+
+
+;; TODO: Consider type of job
+;; TODO: Consider schDecisionTime, [taskDependencies?]
+;; fixture
+;; framework1,arrivalTime //framework name
+;; task1,duration
+;; task2,duration
+;;
+;; e.g
+;; fr1, 0
+;; task1_1,1
+;; task1_2,1
+;; task1_3,1
+;;
+;; fr2, 3
+;; task2_1,1
+;; task2_2,1
+;; task2_3,1
