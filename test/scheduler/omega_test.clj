9b5a32391394196cde6b1dd1720f6dd3396a56d7
txominpelu <imediavilla@viadeoteam.com>
2014-08-18 17:52:42 +0200

    Improving omega tests wip

--- test/scheduler/omega_test.clj
+++ test/scheduler/omega_test.clj
@@ -14,11 +14,9 @@
 
 ;; Test that one framework asks for all cpus and then another framework and see that is the first
 (deftest competeAllCluster-test
-  (testing "when two clusters compete for the whole cluster the first gets it"
+  (testing "when two frameworks compete for the whole cluster the first gets it"
     (let [cluster (cluster/initOmegaCluster omegaIter)
           task (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "fr1" cluster)
-          ;;fr1 (framework/createFramework "fr1" )
-          ;;fr2 (framework/createFramework "fr2" [task])
           demand1 {:resources {:cpus 10} :task task :id 1}
           demand2 {:resources {:cpus 10} :task task :id 2}
          ]
@@ -26,10 +24,37 @@
          (demandResources cluster demand2)
          (let [newCluster (cluster/runIter cluster)]
            (is (= 0 (cluster/getClusterCpus newCluster)))))))
-         ;;(is (= [] (flatten (framework/getClusterTasks (runClusterTillNoTask cluster))))))))
 
 
 ;; Test that both ask for half of the resources and they both get them
+
+(deftest competeAllCluster-test
+  (testing "when two frameworks take half of the cluster"
+    (let [cluster (cluster/initOmegaCluster omegaIter)
+          task (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "fr1" cluster)
+          demand1 {:resources {:cpus 5} :task task :id 1}
+          demand2 {:resources {:cpus 5} :task task :id 2}
+         ]
+         (demandResources cluster demand1)
+         (demandResources cluster demand2)
+         (let [newCluster (cluster/runIter cluster)]
+           (is (= 0 (cluster/getClusterCpus newCluster)))))))
+
+;; Test that finished tasks are taken into account
+(deftest competeAllCluster-test
+  (testing "when two frameworks take half of the cluster"
+    (let [cluster (cluster/initOmegaCluster omegaIter)
+          task (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "fr1" cluster)
+          demand1 {:resources {:cpus 10} :task task :id 1}
+          demand2 {:resources {:cpus 10} :task task :id 2}
+         ]
+         (demandResources cluster demand1)
+         (demandResources cluster demand2)
+         (let [newCluster (cluster/runIter cluster)
+               a (println newCluster)
+               newCluster (cluster/runIter newCluster)]
+           (is (= 10 (cluster/getClusterCpus newCluster)))))))
+
 ;; Test that one framework asks always before the other and always gets the resources (1/2/1/2) -
 ;;   2 always gets refused
 
