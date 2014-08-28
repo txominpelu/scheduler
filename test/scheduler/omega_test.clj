33a206ae83ee4416f3366331b33369bf2ee47e85
Inigo Mediavilla <imediavilla@viadeoteam.com>
2014-08-28 19:49:45 +0200

    Refactored omega tests

--- test/scheduler/omega_test.clj
+++ test/scheduler/omega_test.clj
@@ -15,14 +15,21 @@
              (async/thread (async/>!! (cluster/getDemandsCh cluster) d))
              (Thread/sleep 100))))) ;; FIXME: Ugly!!!
 
+(defn createDemand
+  [cluster] 
+  (fn [{cpus :cpus id :id}]
+    (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) id cluster cpus)))
+
 (defn test-demands
   [demands expSuccess expFailed expCpus]
-  (let [cluster (cluster/initOmegaCluster omegaIter) ]
-        (doall (for [d demands] (demandResources cluster d)))
+  (let [cluster (cluster/initOmegaCluster omegaIter) 
+        cDemand (createDemand cluster)
+        demands (map cDemand demands)]
+        (demandResources cluster demands)
         (let [{newCluster :cluster logs :logs} (cluster/runIter cluster)
               [success failed] (partition-by :success logs)]
-           (is (= (map :demand success) expSuccess))
-           (is (= (map :demand failed)  expFailed))
+           (is (= (map :id (map :demand success)) expSuccess))
+           (is (= (map :id (map :demand failed))  expFailed))
            (is (= expCpus (cluster/getClusterCpus newCluster))))))
 
 
@@ -30,34 +37,17 @@
 ;; Test that one framework asks for all cpus and then another framework and see that is the first
 (deftest competeAllCluster-test
   (testing "when two frameworks compete for the whole cluster the first gets it"
-    (let [cluster (cluster/initOmegaCluster omegaIter)
-          demand1 (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "t1" cluster 10)
-          demand2 (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "t2" cluster 1)
-         ]
-         (demandResources cluster [demand1 demand2])
-         ;;(demandResources cluster demand2)
-         (let [{newCluster :cluster logs :logs} (cluster/runIter cluster)
-               [success failed] (partition-by :success logs)]
-           (is (= (map :demand success) [demand1]))
-           (is (= (map :demand failed)  [demand2]))
-           (is (= 0 (cluster/getClusterCpus newCluster)))))))
-
-
-;; Test that both ask for half of the resources and they both get them
+    (let [demand1 {:id "t1" :cpus 10}
+          demand2 {:id "t2" :cpus 1}]
+      (test-demands [demand1 demand2] ["t1"] ["t2"] 0))))
 
 (deftest halfEach-test
   (testing "when two frameworks take half of the cluster"
-    (let [cluster (cluster/initOmegaCluster omegaIter)
-          demand1 (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "t1" cluster 5)
-          demand2 (cluster/wrapWithNotifyOnFinished (fn [] (println "Run task!")) "t2" cluster 5)
-         ]
-         (demandResources cluster [demand1 demand2])
-         ;;(demandResources cluster demand2)
-         (let [{newCluster :cluster logs :logs} (cluster/runIter cluster)
-               [success failed] (partition-by :success logs)]
-           (is (= (map :demand success) [demand1 demand2]))
-           (is (= (map :demand failed) [])) 
-           (is (= 0 (cluster/getClusterCpus newCluster)))))))
+    (let [demand1 {:id "t1" :cpus 5}
+          demand2 {:id "t2" :cpus 5}]
+      (test-demands [demand1 demand2] ["t1" "t2"] [] 0))))
+
+;; Test that both ask for half of the resources and they both get them
 
 ;; Test that finished tasks are taken into account
 (deftest resourcesRestored-test
